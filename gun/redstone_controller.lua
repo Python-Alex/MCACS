31044d1d5dabb20555845dda669cd890fa4227be
Python-Alex <52885126+Python-Alex@users.noreply.github.com>
2024-11-10 13:04:14 -0800

    Update redstone_controller.lua

--- gun/redstone_controller.lua
+++ gun/redstone_controller.lua
@@ -3,21 +3,22 @@ local colors = require("colors")
 local RedstoneController = {}
 
 function enableSignal(side, color)
+    
     local currentOutput = redstone.getBundledOutput(side)
-    redstone.setBundledOutput(side, currentOutput | color)
+    redstone.setBundledOutput(side, bit32.bar(currentOutput | color))
 
 end
 
 -- Function to clear (turn off) a specific color on a bundled cable
 function clearSignal(side, color)
     local currentOutput = redstone.getBundledOutput(side) or 0
-    redstone.setBundledOutput(side, currentOutput & ~color)
+    redstone.setBundledOutput(side, bit32.band(currentOutput & ~color))
 end
 
 -- Function to check if a specific color is on or off on a bundled cable
 function isSignalOn(side, color)
     local currentOutput = redstone.getBundledOutput(side) or 0
-    return (currentOutput & color) ~= 0
+    return bit32.band(currentOutput & color) ~= 0
 end
 
 -- Function to get the current state of all colors on a bundled cable
